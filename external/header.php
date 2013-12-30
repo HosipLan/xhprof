8c4153968ba78fe5c8543416969bdf6e0272200c
Filip Procházka <filip@prochazka.su>
2013-12-30 21:00:13 +0100

    Removed useless external/footer.php

--- external/header.php
+++ external/header.php
@@ -1,4 +1,5 @@
 <?php
+
 if (PHP_SAPI == 'cli') {
   $_SERVER['REMOTE_ADDR'] = null;
   $_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'];
@@ -51,15 +52,14 @@ if ($controlIPs === false || in_array($_SERVER['REMOTE_ADDR'], $controlIPs) || P
 }
 
 
-//Certain URLs should never have a link displayed. Think images, xml, etc. 
-foreach($exceptionURLs as $url)
-{
+//Certain URLs should never have a link displayed. Think images, xml, etc.
+foreach ($exceptionURLs as $url) {
     if (stripos($_SERVER['REQUEST_URI'], $url) !== FALSE)
     {
         $_xhprof['display'] = false;
         header('X-XHProf-No-Display: Trueness');
         break;
-    }    
+    }
 }
 unset($exceptionURLs);
 
@@ -71,7 +71,7 @@ foreach ($exceptionPostURLs as $url)
     {
         $_xhprof['savepost'] = false;
         break;
-    }    
+    }
 }
 unset($exceptionPostURLs);
 
@@ -83,7 +83,7 @@ if ($_xhprof['doprofile'] === false)
     {
         $_xhprof['doprofile'] = true;
         $_xhprof['type'] = 0;
-    } 
+    }
 }
 unset($weight);
 
@@ -127,7 +127,30 @@ if (extension_loaded('xhprof') && $_xhprof['doprofile'] === true) {
 
 function xhprof_shutdown_function() {
     global $_xhprof;
-    require dirname(__FILE__).'/footer.php';
+
+	if (!defined('XHPROF_LIB_ROOT')) {
+	  define('XHPROF_LIB_ROOT', dirname(dirname(__FILE__)) . '/xhprof_lib');
+	}
+
+	if (extension_loaded('xhprof') && $_xhprof['doprofile'] === true) {
+		$profiler_namespace = $_xhprof['namespace'];  // namespace for your application
+		$xhprof_data = xhprof_disable();
+		$xhprof_runs = new XHProfRuns_Default();
+		$run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace, null, $_xhprof);
+		if ($_xhprof['display'] === true && PHP_SAPI != 'cli')
+		{
+			// url to the XHProf UI libraries (change the host name and path)
+			$profiler_url = sprintf($_xhprof['url'] . '/index.php?run=%s', $run_id, $profiler_namespace);
+			$callGraph_url = sprintf($_xhprof['url'] . '/callgraph.php?run=%s&source=%s', $run_id, $profiler_namespace);
+
+			echo '<span style="position:absolute;top:10px;right:10px;z-index:9999999">',
+					'<a href="'. $profiler_url .'" target="_blank">xhprof</a>',
+				' / ',
+					'<a href="'. $callGraph_url .'" target="_blank">callgraph</a>',
+				'</span>';
+		}
+	}
+
 }
 
 register_shutdown_function('xhprof_shutdown_function');
